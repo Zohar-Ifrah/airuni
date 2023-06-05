2bc9321a1eef2435e760790240f81d5adc60229e
GilShaashua <gilshaashua@gmail.com>
2023-06-05 12:28:25 +0300

    some layout fixes at labels filter and starting implementing the order form at details page

--- frontend/src/cmps/labels-filter.jsx
+++ frontend/src/cmps/labels-filter.jsx
@@ -8,24 +8,14 @@ import { stayService } from '../services/stay.service.local'
 export function LabelsFilter({ onSetFilter }) {
     // eslint-disable-next-line
     const [items, setItems] = useState(stayService.getLabels())
-    // const [selected, setSelected] = useState([])
     const [filterByToEdit, setFilterByToEdit] = useState(stayService.getDefaultFilter())
 
-    // const [position, setPosition] = useState(0)
+    useEffect(() => {
+        onSetFilter(filterByToEdit)
+        // eslint-disable-next-line
+    }, [filterByToEdit])
 
-    // const isItemSelected = (id) => !!selected.find((el) => el === id)
 
-    // const handleClick =
-    //     (id) =>
-    //         ({ getItemById, scrollToItem }) => {
-    //             const itemSelected = isItemSelected(id)
-    //             // console.log(selected)
-    //             setSelected(currentSelected =>
-    //                 itemSelected
-    //                     ? currentSelected.filter((el) => el !== id)
-    //                     : currentSelected.concat(id)
-    //             )
-    //         }
     function handleClick(id) {
         // console.log(id)
         // console.log(items)
@@ -37,75 +27,68 @@ export function LabelsFilter({ onSetFilter }) {
         })
     }
 
-    useEffect(() => {
-        // console.log(selected)
-        onSetFilter(filterByToEdit)
-        // eslint-disable-next-line
-    }, [filterByToEdit])
+    function LeftArrow() {
+        const { isFirstItemVisible, scrollPrev } =
+            React.useContext(VisibilityContext)
+
+        return (
+            <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+                {'<'}
+            </Arrow>
+        )
+    }
+
+    function RightArrow() {
+        const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext)
+
+        return (
+            <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()} style={{ position: 'absolute' }}>
+                {'>'}
+            </Arrow>
+        )
+    }
+
+    function Card({ onClick, selected, title, itemId, labelUrl }) {
+        const visibility = React.useContext(VisibilityContext)
+
+        return (
+            <div
+                onClick={() => onClick(visibility)}
+                tabIndex={0}
+            >
+                <div className="card" >
+                    <img src={labelUrl} alt="title" />
+                    <div style={{ fontSize: '12px' }}>{title}</div>
+                </div>
+            </div>
+        )
+    }
+
+    function Arrow({ children, disabled, onClick }) {
+        return (
+            <button
+                disabled={disabled}
+                onClick={onClick}
+                style={{
+                    opacity: disabled ? "0" : "1",
+                }}
+            >
+                {children}
+            </button>
+        )
+    }
+
     return (
         <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
             {items.map((item) => (
                 <Card
-                    itemId={item.id} // NOTE: itemId is required for track items
+                    itemId={item.id}
                     title={item.title}
                     labelUrl={item.url}
                     key={item.id}
-                    // onClick={handleClick(item.id)}
                     onClick={() => { handleClick(item.id) }}
-                // selected={isItemSelected(item.id)}
                 />
             ))}
         </ScrollMenu>
-    )
-}
-
-function LeftArrow() {
-    const { isFirstItemVisible, scrollPrev } =
-        React.useContext(VisibilityContext)
-
-    return (
-        <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-            {'<'}
-        </Arrow>
-    )
-}
-
-function RightArrow() {
-    const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext)
-
-    return (
-        <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()} style={{ position: 'absolute' }}>
-            {'>'}
-        </Arrow>
-    )
-}
-
-function Card({ onClick, selected, title, itemId, labelUrl }) {
-    const visibility = React.useContext(VisibilityContext)
-
-    return (
-        <div
-            onClick={() => onClick(visibility)}
-            tabIndex={0}
-        >
-            <div className="card" >
-                <img src={labelUrl} alt="title" />
-                <div style={{ fontSize: '12px' }}>{title}</div>
-            </div>
-        </div>
-    )
-}
-
-function Arrow({ children, disabled, onClick }) {
-    return (
-        <button
-            disabled={disabled}
-            onClick={onClick}
-            style={{
-                opacity: disabled ? "0" : "1",
-            }}
-        >
-            {children}
-        </button>
     )
 }
--- frontend/src/cmps/order-form.jsx
+++ frontend/src/cmps/order-form.jsx
@@ -1,63 +1,165 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 export function OrderForm({ stay }) {
-
     const navigate = useNavigate();
+    const [selectionRange, setSelectionRange] = useState({
+        startDate: stay.availableDates[0].startDate,
+        endDate: stay.availableDates[0].endDate,
+        key: 'selection',
+    });
+
+    console.log(selectionRange.startDate);
 
     function calculateAvgReviews() {
-        let count = 0
+        let count = 0;
         const sum = stay.reviews.reduce((acc, review) => {
-            count++
-            return acc + review.rate
-        }, 0)
+            count++;
+            return acc + review.rate;
+        }, 0);
 
-        return sum / count
+        return sum / count;
+    }
+
+    function handleSelect(ranges) {
+        setSelectionRange(ranges.selection);
     }
 
     function onSubmitOrder(ev) {
-        ev.preventDefault()
-        navigate(`/details/${stay._id}/confirm`)
+        ev.preventDefault();
+        navigate(`/details/${stay._id}/confirm`);
     }
 
     return (
         <section className="order-form-container">
-
             <div className="price-rating-container flex space-between">
                 <div className="price-container flex align-center">
-                    <h2> ${stay.price} </h2>
-                    <p> night </p>
+                    <h2>${stay.price}</h2>
+                    <p>night</p>
                 </div>
                 <div className="rating-review-container flex align-center">
-                    {!!stay.reviews.length && <div className="rating-container flex align-center">
-                        <img src="https://res.cloudinary.com/dpbcaizq9/image/upload/v1685704841/star_p6pdqw.svg" alt="Star" />
-                        <p> {`${calculateAvgReviews().toFixed(2)}`} </p>
-                    </div>}
-                    <p>
-                        {` · ${stay.reviews.length} reviews`}
-                    </p>
+                    {!!stay.reviews.length && (
+                        <div className="rating-container flex align-center">
+                            <img
+                                src="https://res.cloudinary.com/dpbcaizq9/image/upload/v1685704841/star_p6pdqw.svg"
+                                alt="Star"
+                            />
+                            <p>{`${calculateAvgReviews().toFixed(2)}`}</p>
+                        </div>
+                    )}
+                    <p>{` · ${stay.reviews.length} reviews`}</p>
                 </div>
             </div>
 
             <form className="flex column align-center" onSubmit={onSubmitOrder}>
-                <label htmlFor="check-in"> check-in </label>
-                <input type="date" name="check-in" id="check-in" required />
+                <div className="dates-guests-container flex column align-center space-between">
+                    <div className="dates-container flex align-center">
+                        <div className="check-in-container">
+                            <span> check-in </span>
+                            <div className='date-check-in'> {selectionRange.startDate} </div>
+                        </div>
+                        <div className="check-out-container">
+                            <span> checkout </span>
+                            <div className='date-check-out'> {selectionRange.endDate} </div>
+                        </div>
+                    </div>
+                    <div className="guests flex column justify-center">
+                        <span> guests </span>
+                        <div className='guests-count'> 1 guest </div>
+                    </div>
+                </div>
+                {/* <div className="date-picker-container">
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        months={2}
+                        minDate={new Date()}
+                        rangeColors={['#FF5E3A']}
+                    />
+                </div> */}
 
-                <label htmlFor="check-out"> check-out </label>
-                <input type="date" name="check-out" id="check-out" required />
-
-                <label htmlFor="guests"> guests </label>
+                {/* <label htmlFor="guests">guests</label>
                 <select name="guests" id="guests">
-                    <option value=""> 1 </option>
-                    <option value=""> 2 </option>
-                    <option value=""> 3 </option>
-                </select>
+                    <option value="">1</option>
+                    <option value="">2</option>
+                    <option value="">3</option>
+                </select> */}
 
-                <button> Reserve </button>
+                <button>Reserve</button>
             </form>
 
-            <p> You won't be charged yet </p>
-
+            <p>You won't be charged yet</p>
         </section>
-    )
+    );
 }
+
+
+
+
+
+// import { useNavigate } from 'react-router-dom';
+
+
+// export function OrderForm({ stay }) {
+
+//     const navigate = useNavigate();
+
+//     function calculateAvgReviews() {
+//         let count = 0
+//         const sum = stay.reviews.reduce((acc, review) => {
+//             count++
+//             return acc + review.rate
+//         }, 0)
+
+//         return sum / count
+//     }
+
+//     function onSubmitOrder(ev) {
+//         ev.preventDefault()
+//         navigate(`/details/${stay._id}/confirm`)
+//     }
+
+//     return (
+//         <section className="order-form-container">
+
+//             <div className="price-rating-container flex space-between">
+//                 <div className="price-container flex align-center">
+//                     <h2> ${stay.price} </h2>
+//                     <p> night </p>
+//                 </div>
+//                 <div className="rating-review-container flex align-center">
+//                     {!!stay.reviews.length && <div className="rating-container flex align-center">
+//                         <img src="https://res.cloudinary.com/dpbcaizq9/image/upload/v1685704841/star_p6pdqw.svg" alt="Star" />
+//                         <p> {`${calculateAvgReviews().toFixed(2)}`} </p>
+//                     </div>}
+//                     <p>
+//                         {` · ${stay.reviews.length} reviews`}
+//                     </p>
+//                 </div>
+//             </div>
+
+//             <form className="flex column align-center" onSubmit={onSubmitOrder}>
+//                 <label htmlFor="check-in"> check-in </label>
+//                 <input type="date" name="check-in" id="check-in" required />
+
+//                 <label htmlFor="check-out"> check-out </label>
+//                 <input type="date" name="check-out" id="check-out" required />
+
+//                 <label htmlFor="guests"> guests </label>
+//                 <select name="guests" id="guests">
+//                     <option value=""> 1 </option>
+//                     <option value=""> 2 </option>
+//                     <option value=""> 3 </option>
+//                 </select>
+
+//                 <button> Reserve </button>
+//             </form>
+
+//             <p> You won't be charged yet </p>
+
+//         </section>
+//     )
+// }
