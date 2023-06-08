44c8d4031c88780fdf9d093c85ae5c6bb5d56d6d
GilShaashua <gilshaashua@gmail.com>
2023-06-08 20:24:21 +0300

    test

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -13,7 +13,7 @@ const rootReducer = combineReducers({
 })
 
 
-const middleware = window?._REDUX_DEVTOOLS_EXTENSION_?.()
+const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
 export const store = createStore(rootReducer, middleware)
 
 
