cbafaae04e04608e27946c46342dd122382c76e1
GilShaashua <gilshaashua@gmail.com>
2023-06-12 16:34:28 +0300

    test

--- frontend/src/cmps/all-reviews.jsx
+++ frontend/src/cmps/all-reviews.jsx
@@ -4,6 +4,13 @@ import { StatisticsReviews } from "./statistics-reviews";
 
 export function AllReviews({ stay }) {
 
+    function getConvertedDate(timestamp) {
+        const date = new Date(timestamp)
+        const month = date.toLocaleString('default', { month: 'long' })
+        const year = date.getFullYear()
+        return `${month} ${year}`
+    }
+
 
     return (
         <section className="all-reviews-container">
@@ -16,6 +23,7 @@ export function AllReviews({ stay }) {
                         <div className="user-details flex align-center">
                             <img src={review.by.imgUrl} alt="" onError={ev => ev.target.src = 'https://res.cloudinary.com/dpbcaizq9/image/upload/v1686066256/user_jsqpzw.png'} />
                             <h3> {review.by.fullname} </h3>
+                            <p> {getConvertedDate(review.at)} </p>
                         </div>
 
                         <div className="txt-container">
