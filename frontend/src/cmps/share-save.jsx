93ee5d35f5cbf5a6f7b9f4ecd845f2f2f2391939
Zohar Ifrah <cpxzoar@gmail.com>
2023-07-23 21:31:34 +0300

    fixed share function + some basic CSS

--- frontend/src/cmps/share-save.jsx
+++ frontend/src/cmps/share-save.jsx
@@ -1,13 +1,34 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { ShareModal } from './share-modal'
 
 
-export function ShareSave() {
+export function ShareSave({stay}) {
 
+    const [isShareModalOpen, setIsShareModalOpen] = useState(false)
+
+    function displayShareModal(isDisplay) {
+        console.log(isDisplay)
+        setIsShareModalOpen(isDisplay)
+    }
+
+    function onOpenModalShowMore(isOpen = true) {
+        setIsShareModalOpen(isOpen)
+    }
 
     return (
         <div className="share-save-container flex align-center">
+            {/* on share btn click */}
+            {isShareModalOpen &&
+                <div>
+                    <div onClick={() => onOpenModalShowMore(false)} className="blur all"></div>
+                    <ShareModal 
+                    stay ={stay}
+                    setIsShareModalOpen={setIsShareModalOpen} />
+                </div>
+            }
             <Link to="#">
-                <div className="share-container flex align-center">
+                <div onClick={() => displayShareModal(true)} className="share-container flex align-center">
                     <img src="https://res.cloudinary.com/dpbcaizq9/image/upload/v1685700527/share_g6x8aa.svg" alt="Share" />
                     <span> Share </span>
                 </div>
