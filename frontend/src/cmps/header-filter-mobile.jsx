24c69391290600e97b5327604ffaf5845d02d2ae
Gil Shaashua <gilshaashua@gmail.com>
2023-08-08 02:13:10 +0300

    some css finishes on header-filter-mobile

--- frontend/src/cmps/header-filter-mobile.jsx
+++ frontend/src/cmps/header-filter-mobile.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react"
-import { SearchMenu } from "./search-menu"
+import { useState } from 'react'
+import { SearchMenu } from './search-menu'
 
 export function HeaderFilterMobile({
     onSetFilter,
     isDetailsShown,
     isHeaderClicked,
 }) {
-
     const [isSearchBarOpen, setIsSearchBarOpen] = useState(false)
     const [focusBtn, setFocusBtn] = useState(null)
     const [barFocused, setBarFocused] = useState(null)
@@ -20,11 +19,15 @@ export function HeaderFilterMobile({
     function isBarFocused(isFocus) {
         // console.log('isFocus: ', isFocus)
         setBarFocused(isFocus)
-      }
+    }
 
     return (
-        <div className="header-filter-mobile-container flex align-center"
-            onClick={() => { onChangeBarDisplay('Search', true) }}>
+        <div
+            className="header-filter-mobile-container flex align-center"
+            onClick={() => {
+                onChangeBarDisplay('Search', true)
+            }}
+        >
             <div className="icon-search">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -46,18 +49,50 @@ export function HeaderFilterMobile({
                 <span>Anywhere</span>
                 <span>Any week • Add guests</span>
             </div>
-            {isSearchBarOpen &&
-                <div className={`search-bar-mobile main-layout ${isSearchBarOpen ? 'search-bar-open' : ''}`}>
+            {isSearchBarOpen && (
+                <div
+                    className={`search-bar-mobile main-layout ${
+                        isSearchBarOpen ? 'search-bar-open' : ''
+                    }`}
+                >
                     <div className={`search-form-menu-container-moblie`}>
-                        <div>X</div>
-                        <SearchMenu onChangeBarDisplay={onChangeBarDisplay}
+                        <button
+                            className="btn-close"
+                            onClick={(ev) => {
+                                ev.stopPropagation()
+                                setIsSearchBarOpen(
+                                    (prevIsSearchBarOpen) => false
+                                )
+                            }}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 32 32"
+                                aria-hidden="true"
+                                role="presentation"
+                                focusable="false"
+                                style={{
+                                    display: 'block',
+                                    fill: 'none',
+                                    height: '12px',
+                                    width: '12px',
+                                    stroke: 'currentcolor',
+                                    strokeWidth: '5.33333',
+                                    overflow: 'visible',
+                                }}
+                            >
+                                <path d="m6 6 20 20M26 6 6 26"></path>
+                            </svg>
+                        </button>
+                        <SearchMenu
+                            onChangeBarDisplay={onChangeBarDisplay}
                             focusBtn={focusBtn}
                             isBarFocused={isBarFocused}
                             isHeaderClicked={isHeaderClicked}
                         />
                     </div>
                 </div>
-            }
+            )}
         </div>
     )
 }
